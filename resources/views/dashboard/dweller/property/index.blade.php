8bac0065e3c9eebff591a561f343dd5ee407aea1
João Marcos Soré de Morais <jaumcj@hotmail.com>
2019-11-05 00:06:33 -0300

    Update Ajustes

--- resources/views/dashboard/dweller/property/index.blade.php
+++ resources/views/dashboard/dweller/property/index.blade.php
@@ -7,20 +7,6 @@
     @foreach ($property->galeries as $key => $item)
       @if($key === 0)
         <img src="/images/{{ $item->src }}" alt="" style="width: 100px; height: 100px">
-      @endif
-    @endforeach
-    {{ $property->name }}
-    {{ $property->description }}
-    @if(!in_array($property->id ,$match))
-      {{ Form::open(array('route' => 'match', 'method' => 'post')) }}
-          {{ Form::hidden('property_id', $property->id) }}
-          {{ Form::submit('Macth') }}
-      {{ Form::close() }}
-    @endif
-
-    @foreach ($property->galeries as $key => $item)
-      @if($key === 0)
-        <img src="/images/{{ $item->src }}" alt="" style="width: 100px; height: 100px">
       @endif 
     @endforeach
     {{ $property->name }}
