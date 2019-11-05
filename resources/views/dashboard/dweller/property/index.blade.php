19a11260dd450311b67c97a05b3d1957565f3e42
João Marcos Soré de Morais <jaumcj@hotmail.com>
2019-11-05 00:05:25 -0300

    Update Ajustes

--- resources/views/dashboard/dweller/property/index.blade.php
+++ resources/views/dashboard/dweller/property/index.blade.php
@@ -4,22 +4,20 @@
 
 @if(!empty($property))
   <div>
-<<<<<<< HEAD
-      @foreach ($property->galeries as $key => $item)
-        @if($key === 0)
-          <img src="/images/{{ $item->src }}" alt="" style="width: 100px; height: 100px">
-        @endif
-      @endforeach
-      {{ $property->name }}
-      {{ $property->description }}
-      @if(!in_array($property->id ,$match))
-        {{ Form::open(array('route' => 'match', 'method' => 'post')) }}
-            {{ Form::hidden('property_id', $property->id) }}
-            {{ Form::submit('Macth') }}
-        {{ Form::close() }}
+    @foreach ($property->galeries as $key => $item)
+      @if($key === 0)
+        <img src="/images/{{ $item->src }}" alt="" style="width: 100px; height: 100px">
       @endif
+    @endforeach
+    {{ $property->name }}
+    {{ $property->description }}
+    @if(!in_array($property->id ,$match))
+      {{ Form::open(array('route' => 'match', 'method' => 'post')) }}
+          {{ Form::hidden('property_id', $property->id) }}
+          {{ Form::submit('Macth') }}
+      {{ Form::close() }}
+    @endif
 
-=======
     @foreach ($property->galeries as $key => $item)
       @if($key === 0)
         <img src="/images/{{ $item->src }}" alt="" style="width: 100px; height: 100px">
@@ -30,7 +28,6 @@
     
     <a href="{{ route('vacancies') }}" class="btn btn-primary">Vagas</a>
    
->>>>>>> bfbeab6ea3f3706b8dc0b722e21b8b63e6a4ec0d
   </div>
 @endif
 
